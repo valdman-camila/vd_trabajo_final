fe1611335937353bdaa2c6503ea4fc07fdbd0c52
Camila Valdman <camilavaldman@outlook.com>
2025-07-03 19:15:05 -0300

    Gato resultado solo aparece al completar ambos juegos

--- src/armarGato/gatoResultado.svelte
+++ src/armarGato/gatoResultado.svelte
@@ -1,38 +1,36 @@
 <script>
-    import {jugadorTipo} from '/src/store.js';
-    import {jugadorColor} from '/src/store.js';
-    import {resultadoJuego1} from '/src/store.js';
-    import {resultadoJuego2} from '/src/store.js';
+  import {jugadorTipo} from '/src/store.js';
+  import {jugadorColor} from '/src/store.js';
+  import {resultadoJuego1} from '/src/store.js';
+  import {resultadoJuego2} from '/src/store.js';
 
-    import Color from "/src/color.svelte";
-    import Mancha from "/src/mancha.svelte";
-    
-    export let altura = 90; // default del juego
-    export let cantidadManchas = 0;
+  import Color from "/src/color.svelte";
+  import Mancha from "/src/mancha.svelte";
 
-    $: serializacion = $jugadorTipo;
-    $: colorJugador = $jugadorColor;
-    $: alturaJugador = $resultadoJuego1;
-    $: manchasJugador = $resultadoJuego2;
-  
+  export let altura = 90;
+  export let cantidadManchas = 0;
+
+  $: serializacion = $jugadorTipo;
+  $: colorJugador = $jugadorColor;
+  $: alturaJugador = $resultadoJuego1;
+  $: manchasJugador = $resultadoJuego2;
 </script>
 
-
-<!-- Este div reemplaza directamente al basket original -->
+{#if alturaJugador !== null && manchasJugador !== null}
 <div class="contenedor">
-    <div class="person-container">
-  <img
-    src={
-      serializacion === "Serializada"
-        ? "/images/gatoDer.svg"
-        : serializacion === "Episodica"
-        ? "/images/gatoIzq.svg"
-        : "/images/linea-gato-dos.svg"
-    }
-    alt="Gato"
-    class="gato"
-    style="height: {alturaJugador}px"
-  />
+  <div class="person-container">
+    <img
+      src={
+        serializacion === "Serializada"
+          ? "/images/gatoDer.svg"
+          : serializacion === "Episodica"
+          ? "/images/gatoIzq.svg"
+          : "/images/linea-gato-dos.svg"
+      }
+      alt="Gato"
+      class="gato"
+      style="height: {alturaJugador}px"
+    />
 
     <div class="person-color">
       <Color tipo={serializacion} tamano={alturaJugador * 0.674} altura={alturaJugador} color={colorJugador} />
@@ -41,8 +39,9 @@
     <div class="manchas">
       <Mancha tipo={serializacion} cant={manchasJugador} tamano={alturaJugador * 0.674} altura={alturaJugador} />
     </div>
+  </div>
 </div>
-</div>
+{/if}
 
 <style>
   .contenedor {
@@ -54,7 +53,7 @@
     padding-bottom: 2rem;
   }
 
-    .person-container {
+  .person-container {
     position: relative;
     height: 360px; 
     width: 100%;
@@ -74,6 +73,7 @@
     z-index: 0;
     transform: translateX(2%);
     bottom: -2%;
+    mix-blend-mode: multiply;
   }
 
   .mancha {
@@ -83,7 +83,8 @@
     border-radius: 50%;
     position: absolute;
   }
-    .person-color {
+
+  .person-color {
     z-index: -1;
     position: absolute;
     transform: translateX(2%);
